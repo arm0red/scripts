eb001c14c8d52641aa4868c6151e2fae42dfa731
Stephen Harris <trackmastersteve@users.noreply.github.com>
2017-11-29 20:49:09 -0800

    Update arm0red.bitcoin.sh

--- arm0red.bitcoin.sh
+++ arm0red.bitcoin.sh
@@ -8,4 +8,5 @@
 
 cat arm0red
 # This script will return the current price of Bitcoin in USD.
+echo -e "\e[31;1m ***** arm0.red Bitcoin Price Tracker *****\e[0m"
 echo "1 BTC = $(curl -s https://api.coindesk.com/v1/bpi/currentprice/usd.json | grep -o 'rate":"[^"]*' | cut -d\" -f3) USD"
