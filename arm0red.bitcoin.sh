fa922fe9aaea54ddb83c25bf2bfea7d10d99c717
Stephen Harris <trackmastersteve@users.noreply.github.com>
2017-11-29 20:45:54 -0800

    Add files via upload

---
+++ arm0red.bitcoin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#                       _____               _  
+#                      |  _  |             | | 
+#   __ _ _ __ _ __ ___ | |/' | _ __ ___  __| | 
+#  / _` | '__| '_ ` _ \|  /| || '__/ _ \/ _` | 
+# | (_| | |  | | | | | \ |_/ /| | |  __/ (_| | 
+#  \__,_|_|  |_| |_| |_|\___(_)_|  \___|\__,_| 
+
+cat arm0red
+# This script will return the current price of Bitcoin in USD.
+echo "1 BTC = $(curl -s https://api.coindesk.com/v1/bpi/currentprice/usd.json | grep -o 'rate":"[^"]*' | cut -d\" -f3) USD"
