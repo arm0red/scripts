fac59326f7e6f5cc9a728e2778f812ecd82355ed
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-08-06 16:29:00 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,3 +14,7 @@ sudo cp backup /usr/local/bin/backup
 
 sudo cp bitcoin /usr/local/bin/bitcoin
 
+sudo cp kali-post-install /usr/local/bin/kali-post-install
+
+sudo cp msfstart /usr/local/bin/msfstart
+
