567f339ec74e6ccd0d09c56404ff7f8cb34dde4f
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-08-09 17:35:59 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+clear
+#                       _____               _  
+#                      |  _  |             | | 
+#   __ _ _ __ _ __ ___ | |/' | _ __ ___  __| | 
+#  / _` | '__| '_ ` _ \|  /| || '__/ _ \/ _` | 
+# | (_| | |  | | | | | \ |_/ /| | |  __/ (_| | 
+#  \__,_|_|  |_| |_| |_|\___(_)_|  \___|\__,_| 
 
-echo "Installing scripts..."
 sudo cp arm0red /usr/local/bin/arm0red
+cat /usr/local/bin/arm0red
+echo -e "\e[31;1m ***** \e[34;1m arm0.red Security \e[31;1m *****\e[0m"
+echo -e "\e[31;1m ***** \e[34;1m Install Script Running \e[31;1m *****\e[0m"
 
 echo "Installing Update Script..."
 sudo cp aptup /usr/local/bin/aptup
