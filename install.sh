25cd8d1df6dd858108e83a990a1329b96422c193
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-08-11 09:29:25 -0700

    added exec flags
    Added exec flags before installing files.

--- install.sh
+++ install.sh
@@ -14,28 +14,36 @@ echo -e "\e[31;1m ***** \e[34;1m arm0.red Security \e[31;1m *****\e[0m"
 echo -e "\e[31;1m ***** \e[34;1m Install Script Running \e[31;1m *****\e[0m"
 
 echo "Installing Update Script..."
+sudo chmod +x aptup
 sudo cp aptup /usr/local/bin/aptup
 #sudo cp dnfup /usr/local/bin/dnfup
 #sudo cp pacup /usr/local/bin/pacup
 
 echo "Installing Backup Script..."
+sudo chmod +x backup
 sudo cp backup /usr/local/bin/backup
 
 echo "Installing Bitcoin Script..."
+sudo chmod +x bitcoin
 sudo cp bitcoin /usr/local/bin/bitcoin
 
 echo "Installing Kali-Post-Install Script..."
+sudo chmod +x kali-post-install
 sudo cp kali-post-install /usr/local/bin/kali-post-install
 
 echo "Installing msfstart Script..."
+sudo chmod +x msfstart
 sudo cp msfstart /usr/local/bin/msfstart
 
 echo "Installing wlan1mon Script..."
+sudo chmod +x wlan1mon
 sudo cp wlan1mon /usr/local/bin/wlan1mon
 
 echo "Installing sysinfo Script..."
+sudo chmod +x sysinfo
 sudo cp sysinfo /usr/local/bin/sysinfo
 
 echo "Installing newhn Script..."
+sudo chmod +x newhn
 sudo cp newhn /usr/local/bin/newhn
 
