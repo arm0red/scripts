6e875469683989ec1b6d5d221eb173f237a941f3
kimmywacky <44282461+kimmywacky@users.noreply.github.com>
2020-10-06 18:00:37 -0700

    Added ncbackup to install list

--- install.sh
+++ install.sh
@@ -43,5 +43,8 @@ sudo cp wlan1mon /usr/local/bin/wlan1mon
 echo -e "\e[31;1m Installing\e[34;1m wlan1managed Script\e[31;1m...\e[0m"
 sudo cp sysinfo /usr/local/bin/wlan1managed
 
+echo -e "\e[31;1m Installing\e[34;1m ncbackup Script\e[31;1m...\e[0m"
+sudo cp ncbackup /usr/local/bin/ncbackup
+
 echo ""
 echo -e "\e[1;32mDone.\e[0m"
